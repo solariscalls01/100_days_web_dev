0b8b26a32f3ac3e6e37596defe441b8a8dc80483
Brian Dy <dyb@oregonstate.edu>
2023-09-06 22:53:52 -0700

    practicing JS

---
+++ Day30-33/app.js
@@ -0,0 +1,17 @@
+let age = 32;
+let userName = "david";
+let hobbies = ["SC", "hiking", "climbing"];
+
+// Objects "dictionary" types
+let person1 = {
+  name: "David",
+  age: 33,
+};
+
+
+function calculateAge (age) {
+    adultYears = age - 18
+    alert(adultYears)
+}
+
+console.log(calculateAge(321))
