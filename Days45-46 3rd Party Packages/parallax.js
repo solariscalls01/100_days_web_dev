7b434dbafce557bf9dec2612678410c5c3ba83f9
Brian Dy <dyb@oregonstate.edu>
2024-01-11 22:51:18 -0800

    finished section

---
+++ Days45-46 3rd Party Packages/parallax.js
@@ -0,0 +1,7 @@
+// using the simpleparralax 3rd party library https://simpleparallax.com/
+
+const image = document.getElementById('main-image');
+new simpleParallax(image, {
+    scale: 1.5, 
+    delay: 0.1,
+});
