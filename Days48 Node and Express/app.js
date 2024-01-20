2c81883b3328b5ce7ee0c94e0c7b2c93c7350d82
Brian Dy <dyb@oregonstate.edu>
2024-01-19 17:20:59 -0800

    filesystem read/ write

--- Days48 Node and Express/app.js
+++ Days48 Node and Express/app.js
@@ -1,25 +1,59 @@
-// Express is technically a function so need to again set the app variable to match the express function
 const express = require('express')
 const app = express()
+const fs = require('fs') //filesystem import
+const path = require('path') //path package can be useeful for setting filepaths
 
 PORT = 3000
 
-// The Overall Idea behind .get is that it creates various pathways for us to use. Instead of the previous module where we had a bunch of 
-// "if URL = someURL, THEN send the response" 
+// App.use requires to load the express.urlencoded() to parse the data
+app.use(express.urlencoded())
 
-
-// the get request handles that will be sent from the server (The Request / Response is AKA Routes or Route Handlers)
-// The first parameter sets the path, the 2nd parameter is the function.
-// In this case, we can use various ways, but in this example we will use the anonomyous function (no variable created)
-app.get("/currenttime", function(req, res) {
-    res.send('<h1>' + new Date().toLocaleDateString() + '</h1>\n'
-    + '<h2>Time:' + new Date().toLocaleTimeString() + '</h2>' )
-} )
-
-// Using the arrow function method
+// Creating a simple form. Just for practice, but obviously wouldn't normally do it like this
 app.get('/', (req,res) => {
-    res.send('<h1>Hello World</h1>')
+    res.send('<form action="/store-user" method="POST"><label>Your Name: </label><input type="text" name="username"><button type="submit">Submit</button></form>')
+})
+
+// The .username was a result from our input tag with the "name". Basically the req.body can request any object from the html page (i believe)
+app.post('/store-user', (req,res) => {
+    const userName = req.body.username
+    console.log(userName)
+
+    // the filepath.join section allows to get the absolute path of the directory
+    // This section tells the filesystem WHERE to write the file. 
+    // path.join with the __dirname allows for crossplatform file pathing for various OSes, 2nd parameter is the directory, 3rd is the file
+    const filePath = path.join(__dirname, 'data', 'users.json')
+    
+    // Before we can write something to a file, we must first read the file first. 
+    const fileData = fs.readFileSync(filePath)
+    const existingUsers = JSON.parse(fileData);
+
+    console.log(fileData)
+    console.log(existingUsers)
+
+    // push method similar to appending in python
+    existingUsers.push(userName);
+   
+    fs.writeFileSync(filePath, JSON.stringify(existingUsers))
+    res.send('<h1>Username Stored!</h1>')
+})
+
+app.get("/users", (req,res) => {
+    const filePath = path.join(__dirname, 'data', 'users.json')
+    
+    // Before we can write something to a file, we must first read the file first. 
+    const fileData = fs.readFileSync(filePath)
+    const existingUsers = JSON.parse(fileData);
+
+    // using a forloop to list all of the items in the users.json file
+    let responseData = '<ul>';
+    for (const users of existingUsers) {
+        responseData += '<li>' + users + '</li>';
+    }
+
+    responseData += '</ul>'
+
+    res.send(responseData)
 })
 
 app.listen(PORT)
-console.log('Listening on ' + PORT)
+console.log("Listening on " + PORT)
