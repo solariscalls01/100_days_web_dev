784ff6a98b9ef79083e1fce563548fd79c2ce96c
Brian Dy <dyb@oregonstate.edu>
2024-01-21 22:48:58 -0800

    set routes and connected CSS to files

--- Days49-51 More Express/app.js
+++ Days49-51 More Express/app.js
@@ -1,13 +1,39 @@
 const express = require ('express')
 const app = express()
+const path = require('path')
 app.use(express.urlencoded())
+
+// anything that is considered a "static page that does not require any dynamic changes will require the express.static method"
+// Since we need to load our CSS and JS files, we need to use this method and parameter is the folder that these files will be stored in. 
+app.use(express.static('public'))
+
 
 const PORT = 3001
 
 
 
 app.get('/', (req,res) => {
-    res.sendFile("C:/Users/solar/Desktop/100 Days of web dev/Days49-51 More Express Static & Dynamic Content/frontend-site/index.html")
+    // Another way to send a file and match pathways
+    res.sendFile(__dirname + '/views/index.html')
+}) 
+
+app.get('/about', (req,res) => {
+    let filePath = path.join(__dirname, 'views', 'about.html')
+    res.sendFile(filePath)
+})
+
+app.get('/recommend', (req,res) => {
+    let filePath = path.join(__dirname, 'views', 'recommend.html')
+    res.sendFile(filePath)
+})
+
+app.get('/restaurants', (req,res) => {
+    let filePath = path.join(__dirname, 'views', 'restaurants.html')
+    res.sendFile(filePath)
+})
+
+app.get('/confirm', (req,res) => {
+    res.sendFile(__dirname + "/views/confirm.html")
 })
 
 
